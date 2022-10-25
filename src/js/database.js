58ae3fea589fe1dd25797fdacca1bf53091b2be9
Filipe520 <filipealves520@gmail.com>
2022-10-25 12:55:18 -0300

    Projeto Faltando Finalizar!

---
+++ src/js/database.js
@@ -0,0 +1,22 @@
+const personagens = [
+    {
+        id: "0",
+        name: "boo",
+        img: "../image/Boo.png",
+    },
+    {
+        id: "1",
+        name: "goomba",
+        img: "../image/Goomba.png",
+    },
+    {
+        id: "2",
+        name: "shell",
+        img: "../image/Shell.png",
+    },
+    {
+        id: "3",
+        name: "piranha",
+        img: "../img/Piranha.png",
+    },
+];
--- src/js/script_game copy.js
+++ src/js/script_game copy.js
@@ -1,13 +1,13 @@
 const div = document.getElementById("cartas")
 
-redenrizarCartas();
+// redenrizarCartas();
 
 function renderizarCartas() {
 //     const arrDuplicado = [...personagens, ...personagens];
 //     const personagensEmbaralhados = shuffleArray(arrDuplicado);
 //     console.log(personagensEmbaralhados);
 
-    for (let i = 0; i < 8; i++) {
+//     for (let i = 0; i < personagensEmbaralhados.length; i++) {
 
 // VAREÁVEIS
 
@@ -20,20 +20,40 @@ function renderizarCartas() {
               // Vai criar a TAG img que vai tá dentro da vareável carta.
         let carta = document.createElement("img");
 
+ //PARÂMETRO
+
+              // Vai adicionar os parâmetros dentro da vareável source01.
         source01.media="(max-width: 417px)";
         source01.srcset="src/image/images_responsivas/bloco-medio.png";
         source01.type="image/png";
 
+              // Vai adicionar os parâmetros dentro da vareável source02.
         source02.media="(max-width: 1024px)";
         source02.srcset="src/image/images_responsivas/bloco-medio.png";
         source02.type="image/png";
 
+        // Vai adicionar os parâmetros dentro da vareável carta.
         carta.src="src/image/images_responsivas/bloco-grande.png";
         carta.alt="Card do Jogo";
         
+//HERDEIROS(FILHOS-TAG)        
+            // Vai adicionar dentro da vareável div as vareáveis
+            //  [1] - Picture
+            //  [2] - Source01
+            //  [3] - Source02
+            //  [4] - Carta
+            //  Tudo isso dentro da div lá em cima.
         div.appendChild(picture);
         picture.appendChild(source01)
         picture.appendChild(source02)
         picture.appendChild(carta)
     }
 }
+
+// function shuffleArray(arr) {
+//     for (let i = arr.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [arr[i], arr[j]] = [arr[j], arr[i]];
+//     }
+//     return arr;
+//   }
